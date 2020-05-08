2698b88b964d93b00d5b23d06a2eb1b8736e3e03
Fabrizio Spataro <fabryprog@gmail.com>
2020-05-08 19:30:03 +0200

    fix word count example

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ This repo contains many JUNIT test classes about Kafka Streaming transformations
 
 ## Contents
 
+## Example:
+ - [Word count example](https://github.com/Fabryprog/kafka-streams-examples/blob/master/src/test/java/it/fabryprog/kafka/streaming/example/transformation/example/WordCountTest.java)
+
+
 ### Stateless
  - [Branch](https://github.com/Fabryprog/kafka-streams-examples/blob/master/src/test/java/it/fabryprog/kafka/streaming/example/transformation/stateless/BranchTest.java)
  - [Filter](https://github.com/Fabryprog/kafka-streams-examples/blob/master/src/test/java/it/fabryprog/kafka/streaming/example/transformation/stateless/FilterTest.java)
@@ -24,6 +28,7 @@ This repo contains many JUNIT test classes about Kafka Streaming transformations
  - TableToStream 
 
 ### Stateful
- - [Word count example](https://github.com/Fabryprog/kafka-streams-examples/blob/master/src/test/java/it/fabryprog/kafka/streaming/example/transformation/stateful/example/WordCountTest.java)
+
+
  
 [Confluent Documentation](https://docs.confluent.io/current/streams/developer-guide/dsl-api.html#streams-developer-guide-dsl-transformations-stateless)
--- src/test/java/it/fabryprog/kafka/streaming/example/transformation/example/WordCountTest.java
+++ src/test/java/it/fabryprog/kafka/streaming/example/transformation/example/WordCountTest.java
@@ -1,4 +1,4 @@
-package it.fabryprog.kafka.streaming.example.transformation.stateful.example;
+package it.fabryprog.kafka.streaming.example.transformation.example;
 
 import it.fabryprog.kafka.streaming.example.AbstractTest;
 import it.fabryprog.kafka.streaming.example.TestInterface;
@@ -18,13 +18,29 @@ public class WordCountTest extends AbstractTest implements TestInterface {
     @Test
     public void shouldTest() {
         final List<String> inputValues = Arrays.asList(
-                "It was the best of times: it was the worst of times",
-                "Love means never having to say you're sorry"
+            "It was the best of times: it was the worst of times",
+            "It is a sunny day"
         );
 
-        // Step 0: Expected output is word count
-        final List<Long> expectedOutput = Arrays.asList(
-                12L, 8L
+        // Step 0: Expected output is a couple of word -> count
+        final List<KeyValue<String, Long>> expectedOutput = Arrays.asList(
+            KeyValue.pair("it", 1L),
+            KeyValue.pair("was", 1L),
+            KeyValue.pair("the", 1L),
+            KeyValue.pair("best", 1L),
+            KeyValue.pair("of", 1L),
+            KeyValue.pair("times", 1L),
+            KeyValue.pair("it", 2L),
+            KeyValue.pair("was", 2L),
+            KeyValue.pair("the", 2L),
+            KeyValue.pair("worst", 1L),
+            KeyValue.pair("of", 2L),
+            KeyValue.pair("times", 2L),
+            KeyValue.pair("it", 3L),
+            KeyValue.pair("is", 1L),
+            KeyValue.pair("a", 1L),
+            KeyValue.pair("sunny", 1L),
+            KeyValue.pair("day", 1L)
         );
 
         try (final TopologyTestDriver testDriver = new TopologyTestDriver(builder.build(), configuration)) {
@@ -40,7 +56,7 @@ public class WordCountTest extends AbstractTest implements TestInterface {
             input.pipeValueList(inputValues);
 
             // Step 3: Validate the output.
-            assertThat(output.readValuesToList(), equalTo(expectedOutput));
+            assertThat(output.readKeyValuesToList(), equalTo(expectedOutput));
         }
     }
 
@@ -49,11 +65,10 @@ public class WordCountTest extends AbstractTest implements TestInterface {
         final StreamsBuilder builder = new StreamsBuilder();
         final KStream<String, String> input = builder.stream(inputTopic, Consumed.with(Serdes.String(), Serdes.String()));
 
-        KTable<String, Long> wordCount = input.map((key, value) -> KeyValue.pair(value, Arrays.asList(value.split("\\W+")))).
+        KTable<String, Long> wordCount = input.flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+"))).
             peek((key, value) -> System.out.println("key=" + key + ", value=" + value)).
-                mapValues(value -> value.size()).
-                groupByKey(Grouped.with(Serdes.String(), Serdes.Integer())).
-                count();
+            groupBy((key, value) -> value, Grouped.with(Serdes.String(), Serdes.String())).
+            count();
 
         wordCount.toStream().to(outputTopic, Produced.with(Serdes.String(), Serdes.Long()));
 
